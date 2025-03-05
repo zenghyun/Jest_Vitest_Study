4f60f0e1aac447733bd5a4f54c169abe3fdb8689
zenghyun <pipo36@naver.com>
2025-03-05 23:58:30 +0900

    1강 공부

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -11,25 +11,25 @@ import App from "./App";
  *
  */
 
-test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn React/i);
-  expect(headingElement).toBeInTheDocument();
-});
-
 // eslint-disable-next-line vitest/no-commented-out-tests
 // test("App contains correct heading", () => {
 //   render(<App />);
-//   //  / / <= 정규 표현식, i <= 대소문자 구분하지 않겠다는 표시
-//   const headingElement1 = screen.getByText(/learn react/i);
-//   const headingElement2 = screen.getByRole("heading", { name: /learn React/i });
-//   // 아래 expect를 통해 테스트가 성공하거나 실패하게 됨
-//   // expect().toBeInTheDocument()
-//   // assertion, causes test to succeed or fail
-//   // headingElement가 이 문서에 있다고 단언한다.
-//   expect(headingElement1).toBeInTheDocument();
-//   expect(headingElement2).toBeInTheDocument();
+//   const headingElement = screen.getByText(/learn React/i);
+//   expect(headingElement).toBeInTheDocument();
 // });
+
+test("App contains correct heading", () => {
+  render(<App />);
+  //  / / <= 정규 표현식, i <= 대소문자 구분하지 않겠다는 표시
+  const headingElement1 = screen.getByText(/learn react/i);
+  const headingElement2 = screen.getByRole("heading", { name: /learn React/i });
+  // 아래 expect를 통해 테스트가 성공하거나 실패하게 됨
+  // expect().toBeInTheDocument()
+  // assertion, causes test to succeed or fail
+  // headingElement가 이 문서에 있다고 단언한다.
+  expect(headingElement1).toBeInTheDocument();
+  expect(headingElement2).toBeInTheDocument();
+});
 
 // eslint-disable-next-line vitest/no-commented-out-tests
 // test("button starts with correct label and color", () => {
