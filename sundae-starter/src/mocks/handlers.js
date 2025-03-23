7edfc381c6c83d7db89c5ae93df8aae1c3231617
zenghyun <pipo36@naver.com>
2025-03-23 14:02:31 +0900

    Topping test 완료

--- sundae-starter/src/mocks/handlers.js
+++ sundae-starter/src/mocks/handlers.js
@@ -14,4 +14,11 @@ export const handlers = [
       },
     ]);
   }),
+  http.get("http://localhost:3030/toppings", () => {
+    return HttpResponse.json([
+      { name: "Cherries", imagePath: "/images/cherries.png" },
+      { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
+      { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
+    ]);
+  }),
 ];
--- sundae-starter/src/pages/entry/test/Options.test.jsx
+++ sundae-starter/src/pages/entry/test/Options.test.jsx
@@ -13,3 +13,20 @@ test("displays image for each scoop option from server", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("Displays image for each toppings option from server", async () => {
+  // Mock Service Worker will return three options from server
+  render(<Options optionType="toppings" />);
+
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  // check the actual alt text for the images
+  // @ts-ignore
+  const imageTitles = images.map((img) => img.alt);
+  expect(imageTitles).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
